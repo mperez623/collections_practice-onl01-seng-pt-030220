c9e9b570a5c682c67b8463d15c75568d0a121227
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 20:06:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,17 @@
 def sort_array_asc(array)
   array.sort do |a, b|
     a <=> b
-end
+  end
 end
 
 def sort_array_desc(array)
   array.sort do |a, b|
     b <=> a
   end
+end
+  
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
   end
+end    
